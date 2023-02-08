51790ceaeb935da9095100381ed7afa1ec95e5f5
Luna Dephensis <engetsu96@gmail.com>
2023-02-08 14:28:03 +0100

    sticky és responsive glass nav hozzáadása

--- script/main.js
+++ script/main.js
@@ -4,3 +4,7 @@ let navigation = document.querySelector('nav');
 menuToggle.addEventListener('click', () => {
     navigation.classList.toggle('active');
 });
+
+window.addEventListener("scroll", () => {
+    navigation.classList.toggle("sticky", window.scrollY > 0);
+});
